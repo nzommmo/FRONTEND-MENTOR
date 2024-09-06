18f4f3992c8a29910beb0f25afc2c14c299b7a5a
nzommmo <ericnzomo17@gmail.com>
2024-09-06 16:17:03 +0300

    Setup

---
+++ Challenge1/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {User} from 'lucide-react'
+
+
+const App = () => {
+  return (
+    <div className='relative'>
+      <div className='container'>
+        <div className='mt-20 flex items-center justify-center'>
+          <div className='rounded border px-20 border-neutral-500'>
+            <div clas>
+            <User/>
+
+            </div>
+            <div>
+              
+            </div>
+
+
+          </div>
+
+        </div>
+
+      </div>
+
+      
+    </div>
+  )
+}
+
+export default App
